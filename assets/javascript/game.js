d9423d6ee5714ba8b1a762bed9923729e43abc66
Jacob Moss <jacobmoss09@gmail.com>
2019-06-16 13:04:00 -0500

    gemstone point values set appropriately at init

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+    //set wins, losses, and users total score to 0
+    var wins;
+    var losses;
+    var userScore;
+    var message;
+    var winningNumber;
+    var gemstoneValues;
+    
+    function init(){
+        wins = 0;
+        losses = 0;
+        userScore = 0;
+        message = "";
+        winningNumber = Math.floor( (Math.random()*120) + 1);
+        $("#game-winning-number").append(winningNumber);
+        $("#wins").text(message);
+        $("#wins").text("Wins: " + wins);
+        $("#losses").text("Losses: " + losses);
+        $("#user-score").text(userScore);
+        $(".gemstone").each(function setgemstonepointvalues(){
+            gemstoneValues = Math.floor( (Math.random()*12) + 1);
+            $(this).attr('pointValue', gemstoneValues);
+        });
+    }
+
+    init();
+
+
+
+
+
+
+
+
+
+
+});
